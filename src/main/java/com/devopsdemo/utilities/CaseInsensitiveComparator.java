53fd4ea06e6e53dafc4f2df9a17186ca0ee91606
AbbA <46997679+Abhishek-Bajpai@users.noreply.github.com>
2020-09-02 15:14:18 -0700

    Removed unnecessary imports

--- src/main/java/com/devopsdemo/utilities/CaseInsensitiveComparator.java
+++ src/main/java/com/devopsdemo/utilities/CaseInsensitiveComparator.java
@@ -2,7 +2,6 @@ package com.devopsdemo.utilities;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
-import java.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
